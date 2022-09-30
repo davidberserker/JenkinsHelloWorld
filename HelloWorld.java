f9ba1c23a968df260bd71b4640e6d9decf1158c9
davidberserker <jiesda.15@gmail.com>
2022-09-30 08:37:34 -0500

    Adding new files

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package Example;
+
+//package Example;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("------*****Hello World & Welcome to Jenkins Training*****-----");
+
+	}
+
+}
